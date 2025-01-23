5b9f57f4183a9adf843401e7261ff6c12c2796d8
BugHunter-X <askzenai@gmail.com>
2025-01-23 22:50:05 +0100

    Add bug.java file

---
+++ bug.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int index = 10; // Index out of bounds
+
+        try {
+            System.out.println(arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException caught!");
+            System.out.println("Message: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
